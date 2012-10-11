0808b38b5d68000052694a9ce52052f23c250c7a
ScottNeaves <scottneaves2014@u.northwestern.edu>
2012-10-10 19:35:51 -0500

    Parse.com is SO BALLIN already got something saved on it.

--- com.eecs394.snapmylife.MainActivity/src/com/eecs394/snapmylife/MainActivity.java
+++ com.eecs394.snapmylife.MainActivity/src/com/eecs394/snapmylife/MainActivity.java
@@ -1,14 +1,21 @@
 package com.eecs394.snapmylife;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class MainActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Parse.initialize(this, "EqLozg930b2WNEi3DXvXk0f8OtwcEktrdz4q7vJq", "RSim6tb6YgDpAyuBobNEe4kQ3EjTIL07Jy43RwPy");
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
         setContentView(R.layout.activity_main);
     }
 
