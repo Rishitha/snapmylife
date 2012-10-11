a92c6a8037a5c35b209bf7fe318d8a7b94f4f96c
Aedan Maher <aedan.gm@gmail.com>
2012-10-10 19:48:20 -0500

    Just a test

--- SnapMyLife/src/com/eecs394/snapmylife/MainActivity.java
+++ SnapMyLife/src/com/eecs394/snapmylife/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
+    // This comment is just to see if I can commit
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
