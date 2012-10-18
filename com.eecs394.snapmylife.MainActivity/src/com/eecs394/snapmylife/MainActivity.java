27ae749ef72519e292556de73816cd647b5f361a
ScottNeaves <scottneaves2014@u.northwestern.edu>
2012-10-17 19:41:34 -0500

    Added http post code, started working on code that open the gallery, but that doesn't work yet.

--- com.eecs394.snapmylife.MainActivity/src/com/eecs394/snapmylife/MainActivity.java
+++ com.eecs394.snapmylife.MainActivity/src/com/eecs394/snapmylife/MainActivity.java
@@ -1,27 +1,91 @@
 package com.eecs394.snapmylife;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 
-import com.parse.Parse;
-import com.parse.ParseObject;
+import com.parse.entity.mime.HttpMultipartMode;
+import com.parse.entity.mime.MultipartEntity;
+import com.parse.entity.mime.content.FileBody;
+import com.parse.entity.mime.content.StringBody;
 
 public class MainActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Parse.initialize(this, "EqLozg930b2WNEi3DXvXk0f8OtwcEktrdz4q7vJq", "RSim6tb6YgDpAyuBobNEe4kQ3EjTIL07Jy43RwPy");
-        ParseObject testObject = new ParseObject("TestObject");
-        testObject.put("foo", "bar");
-        testObject.saveInBackground();
         setContentView(R.layout.activity_main);
     }
+    
+    public void selectPicture() {
+        Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), 1);   	
+    }
+    
+    
 
+    
+    public void postData() {
+        // Create a new HttpClient and Post Header
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpContext localContext = new BasicHttpContext();
+        HttpPost httppost = new HttpPost("http://www.yoursite.com/script.php");
+
+        try {
+        	MultipartEntity entity = new MultipartEntity(HttpMultipartMode.BROWSER_COMPATIBLE);
+        	
+            // Add your data
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(3);
+            nameValuePairs.add(new BasicNameValuePair("id", "12345"));
+            nameValuePairs.add(new BasicNameValuePair("stringdata", "AndDev is Cool!"));
+            nameValuePairs.add(new BasicNameValuePair("image", "image filepath goes here"));
+            
+            for(int index=0; index < nameValuePairs.size(); index++) {
+                if(nameValuePairs.get(index).getName().equalsIgnoreCase("image")) {
+                    // If the key equals to "image", we use FileBody to transfer the data
+                    entity.addPart(nameValuePairs.get(index).getName(), new FileBody(new File (nameValuePairs.get(index).getValue())));
+                } else {
+                    // Normal string data
+                    entity.addPart(nameValuePairs.get(index).getName(), new StringBody(nameValuePairs.get(index).getValue()));
+                }
+            }
+  
+            //httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            httppost.setEntity(entity);
+            
+            // Execute HTTP Post Request
+            HttpResponse response = httpclient.execute(httppost, localContext);
+            
+        } catch (ClientProtocolException e) {
+            // TODO Auto-generated catch block
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+        }
+    } 
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
 }
+
+
