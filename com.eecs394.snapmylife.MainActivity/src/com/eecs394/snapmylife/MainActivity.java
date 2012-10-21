1f3779ebe129bbff0d20961beffb4ca66905735c
ScottNeaves <scottneaves2014@u.northwestern.edu>
2012-10-21 18:20:19 -0500

    attempted reading http response from server, not sure if successful. Also added capability to launch browser in app

--- com.eecs394.snapmylife.MainActivity/src/com/eecs394/snapmylife/MainActivity.java
+++ com.eecs394.snapmylife.MainActivity/src/com/eecs394/snapmylife/MainActivity.java
@@ -2,17 +2,24 @@ package com.eecs394.snapmylife;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.http.HeaderElement;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
+import org.apache.http.ParseException;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HTTP;
 import org.apache.http.protocol.HttpContext;
 
 import android.app.Activity;
@@ -36,6 +43,7 @@ public class MainActivity extends Activity {
 	
 	Uri currImageURI;
 	String currImageStr = "filler";
+	String rstring = "*";
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -92,12 +100,15 @@ public class MainActivity extends Activity {
             nameValuePairs.add(new BasicNameValuePair("username", "user1"));
             nameValuePairs.add(new BasicNameValuePair("password", "password1"));
             nameValuePairs.add(new BasicNameValuePair("file", currImageStr));
+            System.out.println("currImageStr is: ");
+            System.out.println(currImageStr);
             System.out.println("Finished with assigning name-value pairs");
             
             for(int index=0; index < nameValuePairs.size(); index++) {
-                if(nameValuePairs.get(index).getName().equalsIgnoreCase("image")) {
+                if(nameValuePairs.get(index).getName().equalsIgnoreCase("file")) {
                     // If the key equals to "image", we use FileBody to transfer the data
                     entity.addPart(nameValuePairs.get(index).getName(), new FileBody(new File (nameValuePairs.get(index).getValue())));
+                    //entity.addPart(nameValuePairs.get(index).getName(), new FileBody(new File (nameValuePairs.get(index).getValue())));
                 } else {
                     // Normal string data
                     entity.addPart(nameValuePairs.get(index).getName(), new StringBody(nameValuePairs.get(index).getValue()));
@@ -109,8 +120,9 @@ public class MainActivity extends Activity {
             
             // Execute HTTP Post Request
             HttpResponse response = httpclient.execute(httppost, localContext);
+            rstring = getResponseBody(response);
             System.out.println("got http response:");
-            System.out.println(response);
+            System.out.println(rstring);
           
             
         	Context context = getApplicationContext();
@@ -125,6 +137,78 @@ public class MainActivity extends Activity {
             // TODO Auto-generated catch block
         }
     } 
+    
+    public void launchBrowser(View view){
+    	if(rstring=="*"){
+        	Context context = getApplicationContext();
+        	CharSequence text = "Please upload a picture first";
+        	int duration = Toast.LENGTH_SHORT;
+        	Toast toast = Toast.makeText(context, text, duration);
+        	toast.show();
+    	}
+    	else{
+	    	String baseURL = "http://ec2-50-19-152-75.compute-1.amazonaws.com/";
+	    	Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(baseURL + "PythonApp/3/gif/3.gif"));
+	    	startActivity(browserIntent);
+    	}
+    }
+    
+    public static String getResponseBody(HttpResponse response){
+    	String response_text = null;
+    	HttpEntity entity = null;
+    	try{
+    		entity = response.getEntity();
+    		response_text = _getResponseBody(entity);
+    	} catch (ParseException e){
+    		e.printStackTrace();
+    	}catch(IOException e){
+    		if(entity != null){
+    			try {
+    				entity.consumeContent();
+    			}catch(IOException e1){
+    			}
+    		}
+    	}
+    	return response_text;
+    }
+    
+    public static String _getResponseBody(final HttpEntity entity) throws IOException, ParseException {
+    	if(entity == null){throw new IllegalArgumentException("HTTP entity may not be null"); }
+    	InputStream instream = entity.getContent();
+    	if (instream == null) {return "";}
+    	if (entity.getContentLength() > Integer.MAX_VALUE) {throw new IllegalArgumentException("HTTP entity too large to be buffered in memory");}
+    	String charset = getContentCharSet(entity);
+    	if(charset == null){
+    		charset = HTTP.DEFAULT_CONTENT_CHARSET;
+    	}
+    	Reader reader = new InputStreamReader(instream, charset);
+    	StringBuilder buffer = new StringBuilder();
+    	try{
+    		char[] tmp = new char[1024];
+    		int l;
+    		while((l=reader.read(tmp))!=-1){
+    			buffer.append(tmp, 0, 1);
+    		}
+    	}finally {
+    		reader.close();
+    	}
+    	return buffer.toString();
+    }
+    
+    public static String getContentCharSet(final HttpEntity entity) throws ParseException {
+    	if(entity==null) {throw new IllegalArgumentException("HTTP entity may not be null");}
+    	String charset = null;
+    	if (entity.getContentType()!=null){
+    		HeaderElement values[] = entity.getContentType().getElements();
+    		if(values.length>0){
+    			NameValuePair param = values[0].getParameterByName("charset");
+    			if(param!=null){
+    				charset = param.getValue();
+    			}
+    		}
+    	}
+    	return charset;
+    }
     
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
