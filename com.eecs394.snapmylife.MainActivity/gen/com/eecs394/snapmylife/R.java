e53605a5d86e193c4603fea14e295dab0d899d4f
ScottNeaves <scottneaves2014@u.northwestern.edu>
2012-10-28 21:04:57 -0500

    Changed more things 9:05pm 10/28

--- com.eecs394.snapmylife.MainActivity/gen/com/eecs394/snapmylife/R.java
+++ com.eecs394.snapmylife.MainActivity/gen/com/eecs394/snapmylife/R.java
@@ -15,10 +15,18 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int TextView01=0x7f070000;
+        public static final int TextView02=0x7f070002;
+        public static final int btnCancel=0x7f070004;
+        public static final int btnLogin=0x7f070005;
+        public static final int menu_settings=0x7f070006;
+        public static final int txtPwd=0x7f070003;
+        public static final int txtUname=0x7f070001;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_login=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_register=0x7f030002;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
@@ -27,6 +35,7 @@ public final class R {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
+        public static final int title_activity_login=0x7f040004;
         public static final int title_activity_main=0x7f040003;
     }
     public static final class style {
---
+++ com.eecs394.snapmylife.MainActivity/src/com/eecs394/snapmylife/LoginActivity.java
@@ -0,0 +1,117 @@
+package com.eecs394.snapmylife;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
+import org.apache.http.util.EntityUtils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.entity.mime.HttpMultipartMode;
+import com.parse.entity.mime.MultipartEntity;
+import com.parse.entity.mime.content.StringBody;
+
+
+public class LoginActivity extends Activity {
+ 
+ EditText txtUserName;
+ EditText txtPassword;
+ Button btnLogin;
+ Button btnCancel;
+ 
+ /** Called when the activity is first created. */
+ @Override
+ public void onCreate(Bundle savedInstanceState) {
+	 super.onCreate(savedInstanceState);
+	 setContentView(R.layout.activity_login);
+
+	 txtUserName=(EditText)this.findViewById(R.id.txtUname);
+	 txtPassword=(EditText)this.findViewById(R.id.txtPwd);
+	 btnLogin=(Button)this.findViewById(R.id.btnLogin);
+	 btnLogin=(Button)this.findViewById(R.id.btnLogin);
+	 btnLogin.setOnClickListener(new OnClickListener() {
+
+
+		 public void onClick(View v) {
+			 String rspnse = login(txtUserName.getText().toString(), txtPassword.getText().toString()).trim();
+			 if(rspnse.equalsIgnoreCase("True")){
+				 Toast.makeText(LoginActivity.this, "Login Successful",Toast.LENGTH_LONG).show();
+					Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+					startActivity(intent);
+			 }else if(rspnse.equalsIgnoreCase("False")){
+				 Toast.makeText(LoginActivity.this, "Invalid Login",Toast.LENGTH_LONG).show();
+			 }else{
+				 Toast.makeText(LoginActivity.this, "Neither True nor False", Toast.LENGTH_LONG).show();
+			 }
+		 }
+	 });       
+ }
+
+
+
+public String login(String username, String password) {
+	StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+	StrictMode.setThreadPolicy(policy); 
+
+	// Create a new HttpClient and Post Header
+	HttpClient httpclient = new DefaultHttpClient();
+	HttpContext localContext = new BasicHttpContext();
+	HttpPost httppost = new HttpPost("http://ec2-50-19-152-75.compute-1.amazonaws.com/PythonApp/login.py");
+
+	try {
+		MultipartEntity entity = new MultipartEntity(HttpMultipartMode.BROWSER_COMPATIBLE);
+
+
+		// Add your data
+		List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(3);
+		nameValuePairs.add(new BasicNameValuePair("username", username));
+		nameValuePairs.add(new BasicNameValuePair("password", password));
+		System.out.println("Finished with assigning name-value pairs");
+
+		for(int index=0; index < nameValuePairs.size(); index++) {
+				// Normal string data
+				entity.addPart(nameValuePairs.get(index).getName(), new StringBody(nameValuePairs.get(index).getValue()));	
+		}
+
+		//httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+		httppost.setEntity(entity);
+
+		// Execute HTTP Post Request
+		HttpResponse response = httpclient.execute(httppost, localContext);
+		//Get string from server's response
+		HttpEntity httpentity = response.getEntity();
+		String TF = EntityUtils.toString(httpentity);
+		System.out.println("Response is:");
+		System.out.println(TF);
+		return TF;
+
+	} catch (ClientProtocolException e) {
+		return "ClientProtocolException";
+	} catch (IOException e) {
+		return "IOException";
+	}
+
+} 
+
+}
+
+
